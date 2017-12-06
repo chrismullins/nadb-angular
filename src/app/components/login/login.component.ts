6500d3fe0839ab2c6e58bef09697ca395bca6984
Christopher Mullins <christopherrmullins@gmail.com>
2017-12-05 21:43:10 -0500

    Automated ng generate class models/user plus add onLogin() to LoginComponent

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,22 +6,12 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  test: string = 'just a test';
+export class LoginComponent {
+  user: User = new User();
   constructor(private auth: AuthService) {}
-  ngOnInit(): void {
-    let sampleUser: any = {
-      email: 'michael@realpython.com' as string,
-      password: 'michael' as string
-    };
-    this.auth.register(sampleUser)
+  onLogin(): void {
+    this.auth.login(this.user)
     .then((user) => {
-      console.log(user.json());
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-    this.auth.login(sampleUser).then((user) => {
       console.log(user.json());
     })
     .catch((err) => {
